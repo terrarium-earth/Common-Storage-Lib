7299854ff44cabb94e6931e05669c94a3193631b
Alex Nijjar <alexander.nijjar@icloud.com>
2022-11-14 17:10:11 -0600

    forgot to remove something

--- fabric/src/main/java/earth/terrarium/botarium/fabric/fluid/FabricFluidItemHandler.java
+++ fabric/src/main/java/earth/terrarium/botarium/fabric/fluid/FabricFluidItemHandler.java
@@ -6,7 +6,6 @@ import earth.terrarium.botarium.api.item.ItemStackHolder;
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
-import net.fabricmc.fabric.api.transfer.v1.item.base.SingleStackStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.world.item.ItemStack;
@@ -15,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("UnstableApiUsage")
-public record FabricFluidItemHandler(ItemStack stack, ContainerItemContext context, ItemStackStorage storage)  implements PlatformFluidItemHandler {
+public record FabricFluidItemHandler(ItemStack stack, ContainerItemContext context, Storage<FluidVariant> storage)  implements PlatformFluidItemHandler {
 
     public FabricFluidItemHandler(ItemStack stack) {
         this(stack, ItemStackStorage.of(stack));
@@ -28,7 +27,6 @@ public record FabricFluidItemHandler(ItemStack stack, ContainerItemContext conte
     @Override
     public long insertFluid(ItemStackHolder item, FluidHolder fluid, boolean simulate) {
         try (Transaction transaction = Transaction.openOuter()) {
-            storage.
             FabricFluidHolder fabricFluidHolder = FabricFluidHolder.of(fluid);
             long inserted = storage.insert(fabricFluidHolder.toVariant(), fabricFluidHolder.getAmount(), transaction);
             if (!simulate) {
