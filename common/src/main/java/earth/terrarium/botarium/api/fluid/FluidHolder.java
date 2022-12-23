90b58d3fade0291bcc5bc41048ffdb1f5e28f895
gisellevonbingen <gisellevonbingen@gmail.com>
2022-12-23 14:52:21 +0900

    Add fluid translation methods

--- common/src/main/java/earth/terrarium/botarium/api/fluid/FluidHolder.java
+++ common/src/main/java/earth/terrarium/botarium/api/fluid/FluidHolder.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.material.Fluid;
 
 import java.util.Optional;
@@ -80,4 +81,8 @@ public interface FluidHolder {
      * @param tag The {@link CompoundTag} to deserialize from.
      */
     void deserialize(CompoundTag tag);
+
+    Component getTranslationName();
+
+    String getTranslationKey();
 }
