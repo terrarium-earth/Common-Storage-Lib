bfabd2292a5206afd50b4a141671a3cfdfed2498
ThatGravyBoat <thatgravyboat@gmail.com>
2023-07-01 17:11:15 -0230

    Add getDisplayName method to client hook

--- fabric/src/main/java/earth/terrarium/botarium/fabric/extensions/ClientFluidHookImpl.java
+++ fabric/src/main/java/earth/terrarium/botarium/fabric/extensions/ClientFluidHookImpl.java
@@ -4,8 +4,11 @@ import earth.terrarium.botarium.common.fluid.base.FluidHolder;
 import earth.terrarium.botarium.common.fluid.utils.ClientFluidHooks;
 import earth.terrarium.botarium.fabric.fluid.holder.FabricFluidHolder;
 import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.network.chat.Component;
 import net.msrandom.extensions.annotations.ClassExtension;
+import net.msrandom.extensions.annotations.ImplementedByExtension;
 import net.msrandom.extensions.annotations.ImplementsBaseElement;
 
 @SuppressWarnings("UnstableApiUsage")
@@ -20,6 +23,11 @@ public class ClientFluidHookImpl {
     @ImplementsBaseElement
     public static int getFluidColor(FluidHolder fluid) {
         return FluidVariantRendering.getColor(FabricFluidHolder.of(fluid).toVariant());
+    }
+
+    @ImplementedByExtension
+    public static Component getDisplayName(FluidHolder fluid) {
+        return FluidVariantAttributes.getName(FabricFluidHolder.of(fluid).toVariant());
     }
 
 }
