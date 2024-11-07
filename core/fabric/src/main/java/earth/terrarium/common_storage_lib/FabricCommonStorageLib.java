57dde9c9b5a856ec1bc8e26ee61a43c65052016d
rearth <davidus2010@gmail.com>
2024-11-07 20:54:23 +0100

    Initialize callback on server startup on fabric

--- core/fabric/src/main/java/earth/terrarium/common_storage_lib/FabricCommonStorageLib.java
+++ core/fabric/src/main/java/earth/terrarium/common_storage_lib/FabricCommonStorageLib.java
@@ -1,10 +1,21 @@
 package earth.terrarium.common_storage_lib;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class FabricCommonStorageLib implements ModInitializer {
+    
+    private boolean loaded = false;
+    
     @Override
     public void onInitialize() {
-        CommonStorageLib.init();
+        
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            if (loaded) return;
+            loaded = true;
+            CommonStorageLib.init();    // this is required in a later event than initial initialization as registry content might be
+                                        // added by other mods at a later point of initialization
+        });
+        
     }
 }
