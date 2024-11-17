27f1940507939d7d8aa66582d1c601e6566ef356
rearth <davidus2010@gmail.com>
2024-11-17 22:56:32 +0100

    Remove fallback initialization, add self init method for block entities

--- core/fabric/src/main/java/earth/terrarium/common_storage_lib/FabricCommonStorageLib.java
+++ core/fabric/src/main/java/earth/terrarium/common_storage_lib/FabricCommonStorageLib.java
@@ -1,21 +1,13 @@
 package earth.terrarium.common_storage_lib;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class FabricCommonStorageLib implements ModInitializer {
-    
-    private boolean loaded = false;
     
     @Override
     public void onInitialize() {
         
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            if (loaded) return;
-            loaded = true;
-            CommonStorageLib.init();    // this is required in a later event than initial initialization as registry content might be
-                                        // added by other mods at a later point of initialization
-        });
+        CommonStorageLib.init();
         
     }
 }
